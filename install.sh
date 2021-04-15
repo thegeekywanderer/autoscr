2f57ee219f7521d632e5edd78bfa7b6b026a38ad
wolf-hash <karansingh8777@gmail.com>
2021-04-15 11:45:49 +0530

    fixed install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,6 @@ mkdir -p $HOME/.local/share/systemd/user
 sudo cp wifi_mirror.sh /usr/local/bin/
 cp wifi_mirror.service $HOME/.local/share/systemd/user/
 
-sudo systemctl --user daemon-reload
-sudo systemctl --user enable wifi_mirror.service
-sudo systemctl --user start wifi_mirror.service
+systemctl --user daemon-reload
+systemctl --user enable wifi_mirror.service
+systemctl --user start wifi_mirror.service
