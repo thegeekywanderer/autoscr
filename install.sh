a37393544d3757e6fc95e5cceaa5ccead7d25316
wolf-hash <karansingh8777@gmail.com>
2021-04-19 17:32:56 +0530

    renamed to autoscr

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sed -i "s/USER/$USER/g" airscr.service
+sed -i "s/USER/$USER/g" autoscr.service
 
 mkdir -p $HOME/.local/share/systemd/user
 
-sudo cp airscr /usr/local/bin/
-cp airscr.service $HOME/.local/share/systemd/user/
-cp airscr_ip $HOME/.local/share/airscr_ip
+sudo cp autoscr /usr/local/bin/
+cp autoscr.service $HOME/.local/share/systemd/user/
+cp autoscr_ip $HOME/.local/share/autoscr_ip
 
 systemctl --user daemon-reload
-systemctl --user enable airscr.service
-systemctl --user start airscr.service
+systemctl --user enable autoscr.service
+systemctl --user start autoscr.service
--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-systemctl --user disable airscr.service
-systemctl --user stop airscr.service
+systemctl --user disable autoscr.service
+systemctl --user stop autoscr.service
 
-sudo rm /usr/local/bin/airscr
-rm -f $HOME/.local/share/systemd/user/airscr.service
+sudo rm /usr/local/bin/autoscr
+rm -f $HOME/.local/share/systemd/user/autoscr.service
+rm -f $HOME/.local/share/autoscr_ip
 
 systemctl --user daemon-reload
